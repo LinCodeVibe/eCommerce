5394ac753546ab02a20bbd42233b48235beafbc5
LinCodeVibe <hsiaolin29@gmail.com>
2024-06-13 16:34:36 -0700

    Product Slice,Redux,Actions

---
+++ frontend_client/src/redux/store.js
@@ -0,0 +1,9 @@
+import {combineRedecers, combineReducers, configureStore} from '@reduxjs/toolkit';
+
+import product from './slices/product';
+
+const reducer = combineReducers({
+    product,
+});
+
+export default configureStore({ reducer });

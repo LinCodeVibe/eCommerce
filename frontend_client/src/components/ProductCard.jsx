b790cfdfb4db1298495ce75809b22ce66990d2d7
LinCodeVibe <hsiaolin29@gmail.com>
2024-07-03 15:07:42 -0700

    frontend

--- frontend_client/src/components/ProductCard.jsx
+++ frontend_client/src/components/ProductCard.jsx
@@ -1,15 +1,42 @@
-import { Box, Image, Text, Badge, Flex, IconButton, Skeleton } from '@chakra-ui/react';
+import { Box, Image, Text, Badge, Flex, IconButton, Skeleton, useToast, Tooltip } from '@chakra-ui/react';
 import { BiExpand } from 'react-icons/bi';
 import React, { useState } from 'react';
 import { addToFavorites, removeFromFavorites } from '../redux/actions/productActions';
 import { useSelector, useDispatch } from 'react-redux';
 import { MdOutlineFavorite, MdOutlineFavoriteBorder } from 'react-icons/md';
 import { Link as ReactLink } from 'react-router-dom';
+import { addCartItem } from '../redux/actions/cartActions';
+import { useEffect } from 'react';
+import { TbShoppingCartPlus } from 'react-icons/tb';
 
 const ProductCard = ({ product, loading }) => {
 	const dispatch = useDispatch();
 	const { favorites } = useSelector((state) => state.product);
 	const [isShown, setIsShown] = useState(false);
+	const { cartItems } = useSelector((state) => state.cart);
+	const toast = useToast();
+	const [cartPlusDisabled, setCartPlusDisabled] = useState(false);
+
+	useEffect(() => {
+		const item = cartItems.find((cartItem) => cartItem.id === product._id);
+		if (item && item.qty === product.stock) {
+			setCartPlusDisabled(true);
+		}
+	}, [product, cartItems]);
+
+	const addItem = (id) => {
+		if (cartItems.some((cartItem) => cartItem.id === id)) {
+			const item = cartItems.find((cartItem) => cartItem.id === id);
+			dispatch(addCartItem(id, item.qty + 1));
+		} else {
+			dispatch(addCartItem(id, 1));
+		}
+		toast({
+			description: 'Item has been added.',
+			status: 'success',
+			isClosable: true,
+		});
+	};
 
 	return (
 		<Skeleton isLoaded={!loading}>
@@ -29,11 +56,11 @@ const ProductCard = ({ product, loading }) => {
 					height='200px'
 				/>
 				{product.stock < 5 ? (
-					<Badge colorScheme='yellow'> only {product.stock} left</Badge>
+					<Badge colorScheme='yellow'>only {product.stock} left</Badge>
 				) : product.stock < 1 ? (
 					<Badge colorScheme='red'>Sold out</Badge>
 				) : (
-					<Badge colorScheme='green'>In stock</Badge>
+					<Badge colorScheme='green'>In Stock</Badge>
 				)}
 				{product.productIsNew && (
 					<Badge ml='2' colorScheme='purple'>
@@ -52,7 +79,6 @@ const ProductCard = ({ product, loading }) => {
 						${product.price}
 					</Text>
 				</Flex>
-
 				<Flex justify='space-between' mt='2'>
 					{favorites.includes(product._id) ? (
 						<IconButton
@@ -69,13 +95,34 @@ const ProductCard = ({ product, loading }) => {
 							onClick={() => dispatch(addToFavorites(product._id))}
 						/>
 					)}
+
 					<IconButton
-						icon={<BiExpand size='20px' />}
+						icon={<BiExpand size='20' />}
 						as={ReactLink}
 						to={`/product/${product._id}`}
 						colorScheme='cyan'
 						size='sm'
 					/>
+
+					<Tooltip
+						isDisabled={!cartPlusDisabled}
+						hasArrow
+						label={
+							!cartPlusDisabled
+								? 'You reached the maximum quantity jof the product. '
+								: product.stock <= 0
+								? 'Out of stock'
+								: ''
+						}
+					>
+						<IconButton
+							isDisabled={product.stock <= 0 || cartPlusDisabled}
+							onClick={() => addItem(product._id)}
+							icon={<TbShoppingCartPlus size='20' />}
+							colorScheme='cyan'
+							size='sm'
+						/>
+					</Tooltip>
 				</Flex>
 			</Box>
 		</Skeleton>
--- frontend_client/src/redux/actions/productActions.js
+++ frontend_client/src/redux/actions/productActions.js
@@ -1,73 +1,84 @@
-import { setProducts, setLoading, setError, setPagination, setFavorites, setFavoritesToggle, setProduct } from '../slices/product';
+import {
+	setProducts,
+	setLoading,
+	setError,
+	setPagination,
+	setFavorites,
+	setFavoritesToggle,
+	setProduct,
+} from '../slices/product';
 import axios from 'axios';
 
 export const getProducts = (page, favoritesToggle) => async (dispatch) => {
-    dispatch(setLoading());
-    try {
-        const { data } = await axios.get(`/api/products/${page}/${10}`);
-        const { products, pagination } = data;
-        dispatch(setProducts(products));
-        dispatch(setPagination(pagination));
-     } catch (error) {
-            dispatch(setError(
-                error.reponse && error.reponse.data.message 
-                ? error.response.data.message 
-                : error.message 
-                ? error.message
-                : 'An expected error has ocurred. Please try again later.'
+	dispatch(setLoading());
+	try {
+		const { data } = await axios.get(`/api/products/${page}/${10}`);
+		const { products, pagination } = data;
+		dispatch(setProducts(products));
+		dispatch(setPagination(pagination));
+	} catch (error) {
+		dispatch(
+			setError(
+				error.reponse && error.reponse.data.message
+					? error.response.data.message
+					: error.message
+					? error.message
+					: 'An expected error has ocurred. Please try again later.'
+			)
+		);
+	}
+};
 
-            ));
-        }
-    };
+export const addToFavorites = (id) => async (dispatch, getState) => {
+	const {
+		product: { favorites },
+	} = getState();
 
-    export const addToFavorites = (id) => async (dispatch, getState) => {
-        const {
-            product: { favorites},
-        } = getState();
+	const newfavorites = [...favorites, id];
+	localStorage.setItem('favorites', JSON.stringify(newfavorites));
+	dispatch(setFavorites(newfavorites));
+};
 
-        const newfavorites = [...favorites, id]
-        localStorage.setItem('favorites', JSON.stringify(newfavorites));
-        dispatch(setFavorites(newfavorites));
-    };
+export const removeFromFavorites = (id) => async (dispatch, getState) => {
+	const {
+		product: { favorites },
+	} = getState();
 
-    export const removeFromFavorites = (id) => async (dispatch, getState) => {
-        const {
-            product: { favorites},
-        } = getState();
+	const newfavorites = favorites.filter((favoriteId) => favoriteId !== id);
+	localStorage.setItem('favorites', JSON.stringify(newfavorites));
+	dispatch(setFavorites(newfavorites));
+};
 
-        const newfavorites = favorites.filter((favoriteId) => favoriteId !== id);
-        localStorage.setItem('favorites', JSON.stringify(newfavorites));
-        dispatch(setFavorites(newfavorites));
-    };
+export const toggleFavorites = (toggle) => async (dispatch, getState) => {
+	const {
+		product: { favorites, products },
+	} = getState();
 
-    export const toggleFavorites = (toggle) => async (dispatch, getState) => {
-        const {
-            product: { favorites, products},
-        } = getState();
+	if (toggle) {
+		const filterProducts = products.filter((product) => favorites.includes(product._id));
+		dispatch(setFavoritesToggle(toggle));
+		dispatch(setProducts(filterProducts));
+	} else {
+		dispatch(setFavoritesToggle(false));
+		dispatch(getProducts(1));
+	}
+};
 
-        if(toggle) {
-            const filterProducts = products.filter((product) => favorites.includes(product._id));
-            dispatch(setFavoritesToggle(toggle));
-            dispatch(setProducts(filterProducts));
-        } else {
-            dispatch(setFavoritesToggle(false));
-            dispatch(getProducts(1));
-        }
-    };
-
-    // function calling the product
-    export const getProduct = (id) => async (dispatch) => {
-        dispatch(setLoading(true));
-        try {
-            const {data} = await axios.get(`/api/products/${id}`);
-            dispatch(setProduct(data));
-        } catch (error) {
-            dispatch(setError(
-                error.reponse && error.reponse.data.message 
-                ? error.response.data.message 
-                : error.message 
-                ? error.message
-                : 'An expected error has ocurred. Please try again later.'
-            ));
-        }
-    };
+// function calling the product
+export const getProduct = (id) => async (dispatch) => {
+	dispatch(setLoading(true));
+	try {
+		const { data } = await axios.get(`/api/products/${id}`);
+		dispatch(setProduct(data));
+	} catch (error) {
+		dispatch(
+			setError(
+				error.reponse && error.reponse.data.message
+					? error.response.data.message
+					: error.message
+					? error.message
+					: 'An expected error has ocurred. Please try again later.'
+			)
+		);
+	}
+};

ca2660cf8f5d003f22ca11c8697c188527bf90fe
LinCodeVibe <hsiaolin29@gmail.com>
2024-07-09 09:05:42 -0700

    Email Verificationl

--- backend_server/routes/userRoutes.js
+++ backend_server/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import User from '../models/User.js';
 import expressAsyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
+import { sendVerificationEmail } from '../middleware/sendVerificationEmail.js';
 
 const userRoutes = express.Router();
 
@@ -53,6 +54,8 @@ const registerUser = expressAsyncHandler(async (req, res) => {
 	});
 
 	const newToken = genToken(user._id);
+
+	sendVerificationEmail(newToken, email, name, user._id);
 
 	if (user) {
 		res.status(201).json({
